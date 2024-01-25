74fd31da54ed4f680e4dc9ed5639440428871d47
Abhisek <abhisekmaiti19@gmail.com>
2024-01-26 03:30:46 +0530

    hello

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { Outlet } from "react-router";
+export default function App() {
+  return (
+    <div className="w-screen h-screen">
+      <Outlet />
+    </div>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Landing from "./pages/Landing.jsx";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="" element={<App />}>
+      <Route path="" element={<Landing />}></Route>
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={routes} />
+  </React.StrictMode>
+);
