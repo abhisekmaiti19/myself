859a3fa905855f693402ba05d99c2135c1ef5e6f
Abhisek <abhisekmaiti19@gmail.com>
2024-02-06 03:18:57 +0530

    cursor

--- src/App.jsx
+++ src/App.jsx
@@ -7,37 +7,41 @@ export default function App() {
       <div className="w-full max-w-[1496px] h-screen overflow-y-scroll">
         <Outlet />
       </div>
-      <AnimatedCursor
-        innerSize={8}
-        outerSize={35}
-        innerScale={1}
-        outerScale={2}
-        outerAlpha={0}
-        hasBlendMode={true}
-        clickables={[
-          "a",
-          'input[type="text"]',
-          'input[type="email"]',
-          'input[type="number"]',
-          'input[type="submit"]',
-          'input[type="image"]',
-          "label[for]",
-          "select",
-          "textarea",
-          "button",
-          ".link",
-          "svg",
-          "img",
-        ]}
-        innerStyle={{
-          backgroundColor: "#6D6E71",
-        }}
-        outerStyle={{
-          border: "3px solid #fff",
-          backgroundColor: "#fff",
-          mixBlendMode: "exclusion",
-        }}
-      />
+      {window.innerWidth > 768 ? (
+        <AnimatedCursor
+          innerSize={8}
+          outerSize={35}
+          innerScale={1}
+          outerScale={2}
+          outerAlpha={0}
+          hasBlendMode={true}
+          clickables={[
+            "a",
+            'input[type="text"]',
+            'input[type="email"]',
+            'input[type="number"]',
+            'input[type="submit"]',
+            'input[type="image"]',
+            "label[for]",
+            "select",
+            "textarea",
+            "button",
+            ".link",
+            "svg",
+            "img",
+          ]}
+          innerStyle={{
+            backgroundColor: "#6D6E71",
+          }}
+          outerStyle={{
+            border: "3px solid #fff",
+            backgroundColor: "#fff",
+            mixBlendMode: "exclusion",
+          }}
+        />
+      ) : (
+        ""
+      )}
     </div>
   );
 }
